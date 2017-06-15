787953369dfaeddf89d908059fe1cb6b7c3d9155
Manny Salcido <Manny Salcido>
2017-06-15 12:29:15 -0500

    6-15, Front End Design and Development

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,4 +8,46 @@ $(document).ready(function(){
         infinite: true
       });
 
+
+    var zip;
+    var email;
+    var distance;
+    var interest;
+    var date;
+
+    // var pics = ['../img/concert.jpg', '../img/hang_out3.jpg', '../img/rock_climbing.jpg']
+
+    $('#submit').on('click', function (e) {
+    	e.preventDefault();
+
+    	$('#formHolder').slideUp(3000);
+
+    });
+
+    var form = $('#formHolder');
+    var backgrounds = ['url("assets/img/concert.jpg") 0 0 no-repeat', 'url("assets/img/hang_out3.jpg") 0 0 no-repeat', 'url("assets/img/rock_climbing.jpg") 0 0 no-repeat'];
+    var current = 0;
+    
+
+    function nextBackground() {
+    	
+
+        form.css({
+            'background': backgrounds[current = ++current % backgrounds.length],
+            'background-size': '100%'
+
+    });
+
+        setTimeout(nextBackground, 10000);
+    }
+
+    
+    setTimeout(nextBackground, 10000);
+    form.css('background', backgrounds[0]);
+
+
+
+
+
+
 }); // END READY
