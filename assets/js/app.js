453156f1bc8f306d1bb72a56919816d7c6ffd9bb
Phillip Chu <phillip.chu1@gmail.com>
2017-06-17 10:11:16 -0500

    Added firebase and google maps api

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,12 +8,70 @@ $(document).ready(function(){
         infinite: true
       });
 
-
     var zip;
     var email;
     var distance;
     var interest;
     var date;
+
+    // Beginning code for firebase
+    var config = {
+    apiKey: "AIzaSyDp5uLcLT626edyPAnj3wNW-H4ZS1m3JZc",
+    authDomain: "eventability-4e0eb.firebaseapp.com",
+    databaseURL: "https://eventability-4e0eb.firebaseio.com/",
+    projectId: "eventability-4e0eb",
+    storageBucket: "eventability-4e0eb.appspot.com",
+    messagingSenderId: "52655380758"
+    };
+    
+    firebase.initializeApp(config);
+
+    var database = firebase.database();
+
+    $("#submit").on("click", function(event) {
+        event.preventDefault();
+
+        zip = $("#zip").val().trim();
+        email = $("#email").val().trim();
+        interest = $("#category").val();
+
+        database.ref().push({
+            zip: zip,
+            email: email,
+            interest: interest
+        });
+    });
+
+    database.ref().on("value", function(snapshot) {
+
+        if (snapshot === undefined) {
+
+            database.ref().push({
+                zip: zip,
+                email: email,
+                interest: interest
+            });
+        }
+        else {
+            var sv = snapshot.val();
+            var svArr = Object.keys(sv);
+            var lastIndex = svArr.length - 1;
+            var lastKey = svArr[lastIndex];
+            var lastObj = sv[lastKey];
+
+            console.log(lastObj.zip);
+            console.log(lastObj.email);
+            console.log(lastObj.interest);
+
+            console.log(snapshot.val());
+        }
+    }, function(errorObject) {
+        console.log("Errors handled: " + errorObject.code);
+    });
+
+    // End code for firebase
+
+        console.log(interest);
 
     // var pics = ['../img/concert.jpg', '../img/hang_out3.jpg', '../img/rock_climbing.jpg']
 
